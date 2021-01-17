8a96f36b443abf558ccc9e8838ee430c79291862
chehekb10 <72211025+chehekb10@users.noreply.github.com>
2021-01-17 16:08:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function getParagraph1()
+{
+    var inputs = [];
+for(var  i = 1 ; i<=6 ; i++)
+{
+   inputs.push(document.getElementById("para1_INPUT_BOX_" + i).value);
+}
+
+document.getElementById("showParagraph1").innerHTML =  inputs.join(". ");
+}
+function getParagraph2()
+{
+    var inputs = [];
+for(var  i = 1 ; i<=6 ; i++)
+{
+   inputs.push(document.getElementById("para2_INPUT_BOX_" + i).value);
+}
+
+document.getElementById("showParagraph2").innerHTML =  inputs.join(". ");
+}
+
+
